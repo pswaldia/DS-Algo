182b4127419e8c732ca314e1ee82119989b62536
Pradeep Singh <pswaldia1@gmail.com>
2020-04-29 00:30:23 +0530

    Create MaximumSldingWindow.cpp

---
+++ Interview_Problems/MaximumSldingWindow.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+	vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+		vector<int> result;
+		deque<int> dq;
+		int n = nums.size();
+		for (int i = 0; i < n; i++) {
+			while (!dq.empty() && (i - dq.front() >= k))
+				dq.pop_front();
+			while (!dq.empty() && (nums[i] > nums[dq.back()]))
+				dq.pop_back();
+			dq.push_back(i);
+			if (i >= k - 1)
+				result.push_back(nums[dq.front()]);
+		}
+		return result;
+	}
+};
